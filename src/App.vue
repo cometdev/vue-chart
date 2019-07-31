b50838307a62fda1a82078333968ebbf67e80e3b
Park HyeongYun <phy1103@gmail.com>
2019-07-31 19:51:17 +0900

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <bar-chart :chartdata="barChartData"></bar-chart>
+    <line-chart></line-chart>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LineChart from './components/LineChart.vue';
+import BarChart from './components/BarChart.vue';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	components: {
+		BarChart,
+		LineChart,
+	},
+	data() {
+		return {
+			barChartData: [],
+		};
+	},
+	methods: {
+		fetchChartData() {
+			return new Promise(function(resolve, reject) {
+				setTimeout(function() {
+					console.log('3초 뒤 실행');
+					let result = [
+						{
+							label: '# of Votes',
+							data: [12, 19, 3, 5, 2, 3],
+							backgroundColor: [
+								'rgba(255, 99, 132, 0.2)',
+								'rgba(54, 162, 235, 0.2)',
+								'rgba(255, 206, 86, 0.2)',
+								'rgba(75, 192, 192, 0.2)',
+								'rgba(153, 102, 255, 0.2)',
+								'rgba(255, 159, 64, 0.2)',
+							],
+							borderColor: [
+								'rgba(255, 99, 132, 1)',
+								'rgba(54, 162, 235, 1)',
+								'rgba(255, 206, 86, 1)',
+								'rgba(75, 192, 192, 1)',
+								'rgba(153, 102, 255, 1)',
+								'rgba(255, 159, 64, 1)',
+							],
+							borderWidth: 1,
+						},
+					];
+					resolve(result);
+				}, 3000);
+			});
+		},
+	},
+	// created() {
+	// 	// this.renderChart();
+	// },
+	created() {
+		console.log(this);
+		this.fetchChartData()
+			.then(data => {
+				console.log(data);
+				this.barChartData = data;
+			})
+			.catch(() => {
+				console.log('error');
+			});
+	},
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/BarChart.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <canvas ref="myChart" id="myChart" width="400" height="400"></canvas>
+  </div>
+</template>
+
+<script>
+// import Chart from 'chart.js';
+
+export default {
+	props: {
+		chartdata: {
+			type: Array,
+			default: [],
+		},
+	},
+	watch: {
+		// 1
+		chartdata(newValue, oldValue) {
+			console.log('received');
+			this.renderChart();
+		},
+
+		// 2
+		// chartdata: {
+		// 	handler(newValue, oldValue) {
+		// 		this.renderChart();
+		// 	},
+		// 	// immediate: true,
+		// },
+	},
+	methods: {
+		renderChart() {
+			let ctx = this.$refs.myChart.getContext('2d');
+			// var ctx = document.getElementById('myChart').getContext('2d');
+			var myChart = new this.$_Chart(ctx, {
+				type: 'bar',
+				data: {
+					labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+					datasets: this.chartdata,
+				},
+				options: {
+					scales: {
+						yAxes: [
+							{
+								ticks: {
+									beginAtZero: true,
+								},
+							},
+						],
+					},
+				},
+			});
+		},
+	},
+	// mounted() {
+	// 	this.renderChart();
+	// },
+};
+</script>
+
+<style>
+</style>
